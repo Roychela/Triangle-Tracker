a1739bfaf708bd12ea5acba3ee47a93ad88972bb
Roy Chela <roychela@gmail.com>
2019-04-28 14:19:33 +0300

    Add else if statement to check whether a triangle is scalene.

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,7 @@ function triangle() {
     else if (side2===side3 && side2!==side1) {
       alert ("This is an Isosceles triangle");
   }
+    else if (side1!==side2 && side2!==side3) {
+      alert("This is a scalene triangle");
+  }
 }
