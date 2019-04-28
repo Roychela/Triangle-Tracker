a45a53c796ada7f66436cd1ac6e8e52e9f3b9352
Roy Chela <roychela@gmail.com>
2019-04-28 14:06:26 +0300

    Add three else if statements to check whether a triangle is Isosceles.

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,13 @@ function triangle() {
 
   if (side1===side2 && side1===side3) {
     alert("This is an equilateral triangle");
+  } else if (side1===side2 && side1!==side3) {
+      alert ("This is an Isosceles triangle");
+  }
+    else if (side1===side3 && side1!==side2) {
+      alert ("This is an Isosceles triangle");
+  }
+    else if (side2===side3 && side2!==side1) {
+      alert ("This is an Isosceles triangle");
   }
 }
