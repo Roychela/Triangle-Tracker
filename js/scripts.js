69af5d0881f783adc49e5b2e2124a2c4328753ec
Roy Chela <roychela@gmail.com>
2019-04-28 13:47:48 +0300

    Add javascript function with variables assigned to input values from html input fields

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function triangle() {
+  var side1 = parseInt(document.getElementById("sideAB").value);
+  var side2 = parseInt(document.getElementById("sideAC").value);
+  var side3 = parseInt(document.getElementById("sideBC").value);
+
+  var sumTwoSides1 = (side1+side2);
+  var sumTwoSides2 = (side1+side3);
+  var sumTwoSides3 = (side2+side3);
+}
